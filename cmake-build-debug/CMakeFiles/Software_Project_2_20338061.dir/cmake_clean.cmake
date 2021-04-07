2aa72497112f556e7169a3489dc9270e599cd9f3
Ayomide Sola-Ayodele <ayomide.sola-ayodele@ucdconnect.ie>
2021-04-07 20:04:50 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Software_Project_2_20338061.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Software_Project_2_20338061.dir/main.c.obj"
+  "Software_Project_2_20338061.exe"
+  "Software_Project_2_20338061.exe.manifest"
+  "Software_Project_2_20338061.pdb"
+  "libSoftware_Project_2_20338061.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Software_Project_2_20338061.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Software_Project_2_20338061.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Software_Project_2_20338061.
+# This may be replaced when dependencies are built.
